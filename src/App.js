483d0d9add6e5cd680aa24665ba20eac8dddea71
Junaid <33651074+jaslam94@users.noreply.github.com>
2021-11-08 19:32:01 +0500

    added a card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-6xl">Hello World!</h1>
-    </div>
+    <>
+      <div className="max-w-sm rounded shadow-lg overflow-hidden">
+        <img
+          src="https://source.unsplash.com/random"
+          alt="book-cover"
+          className="w-full"
+        />
+        <div className="px-6 py-4">
+          <div className="font-bold text-purple-500 mb-2 text-lg">
+            Book title
+          </div>
+          <ul>
+            <li>
+              <strong>Description:</strong>{" "}
+            </li>
+            <li>
+              <strong>Authors:</strong>{" "}
+            </li>
+            <li>
+              <strong>Language:</strong>{" "}
+            </li>
+            <li>
+              <strong>Published:</strong>{" "}
+            </li>
+            <li>
+              <strong>Publisher:</strong>{" "}
+            </li>
+          </ul>
+        </div>
+        <div className="px-6 py-4">
+          <span className="inline-block bg-gray-50 rounder-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            tag1
+          </span>
+          <span className="inline-block bg-gray-50 rounder-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            tag2
+          </span>
+          <span className="inline-block bg-gray-50 rounder-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            tag3
+          </span>
+        </div>
+      </div>
+    </>
   );
 }
 
