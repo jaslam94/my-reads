8226d35fb74613533a9d2ee48848112206e8c9b2
Junaid <33651074+jaslam94@users.noreply.github.com>
2021-11-19 14:08:26 +0500

    added db.js

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 dotenv.config({ path: "./config/config.env" });
+
+connectDB();
 
 const auth = require("./routes/auth");
 const books = require("./routes/books");
 const users = require("./routes/users");
-
 const app = express();
 
 app.use("/api/v1/auth", auth);
