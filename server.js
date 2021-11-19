000a51a169ee27e52376ac894c1e95bd780fede5
Junaid <33651074+jaslam94@users.noreply.github.com>
2021-11-19 12:04:31 +0500

    added routes and router in server.js

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ const morgan = require("morgan");
 
 dotenv.config({ path: "./config/config.env" });
 
+const auth = require("./routes/auth");
+const books = require("./routes/books");
+const users = require("./routes/users");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello"));
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/books", books);
+app.use("/api/v1/users", users);
 
 const PORT = process.env.PORT || 5000;
 
