5fb4e9077662c0c3febca956e9e08a4f006698ad
Junaid <33651074+jaslam94@users.noreply.github.com>
2021-11-19 12:18:25 +0500

    added methods and routes to books

--- controllers/books.js
+++ controllers/books.js
@@ -1,1 +1,12 @@
-exports.getBooks = (req, res, next) => res.send("Get books");
+exports.getBooks = (req, res, next) => {
+  res.send("Get my books");
+};
+exports.addWantToRead = (req, res, next) => {
+  res.send("Add to want to read books");
+};
+exports.addRead = (req, res, next) => {
+  res.send("Add to read books");
+};
+exports.delBook = (req, res, next) => {
+  res.send("Del book");
+};
--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { getBooks } = require("../controllers/books");
+const {
+  getBooks,
+  addWantToRead,
+  addRead,
+  delBook,
+} = require("../controllers/books");
 
 router.route("/").get(getBooks);
+router.route("/wanttoread").post(addWantToRead);
+router.route("/read").post(addRead);
+router.route("/").delete(delBook);
 
 module.exports = router;
