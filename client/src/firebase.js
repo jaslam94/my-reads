5fb06d55b8a32a1ccb2dfeb9823fca47dca65ac7
Junaid <33651074+jaslam94@users.noreply.github.com>
2021-12-14 15:10:55 +0500

    unhid the firebase

---
+++ client/src/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDkTq66QWKFyCERBGK-0GmfSxeng3E8J5k",
+  authDomain: "my-reads-bc2a1.firebaseapp.com",
+  projectId: "my-reads-bc2a1",
+  storageBucket: "my-reads-bc2a1.appspot.com",
+  messagingSenderId: "18366930519",
+  appId: "1:18366930519:web:3782abaddfabbb8b904d12",
+  measurementId: "G-5HB4P50G5F",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+
+export { auth, provider };
